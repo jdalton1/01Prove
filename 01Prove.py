4554d0afa7da8e5483638261dc154990a99c5f2a
Josh Dalton <dal17007@byui.edu>
2022-01-12 14:26:16 -0700

    added board creation and display

--- 01Prove.py
+++ 01Prove.py
@@ -4,13 +4,25 @@ Author: Josh Dalton
 '''
 
 def main():
-    return 0
+    player = next_player("")
+    board = new_board()
+
+    return 
 
 def new_board():
-    return
+    board = []
+    for square in range(9):
+        board.append(square + 1)
+    return board
 
-def display_board():
-    return
+def display_board(board):
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print("--+--+--")
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print("--+--+--")
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
 
 def cats_game():
     return
