af25e7a4471013d3fb1c15256c34207bdaa1ce1b
Josh Dalton <dal17007@byui.edu>
2022-01-12 14:20:30 -0700

    function stubs

--- 01Prove.py
+++ 01Prove.py
@@ -1,5 +1,27 @@
 '''
-Solo Checkpoint 02
+Solo Checkpoint 01
 Author: Josh Dalton
 '''
+
+def main():
+    return 0
+
+def new_board():
+    return
+
+def display_board():
+    return
+
+def cats_game():
+    return
+
+
+def game_won():
+    return
+
+def take_turn():
+    return
+
+def next_player():
+    return
 
